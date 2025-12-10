b7ddff0aace79066e620555b619a0d8ec8bfa314
Diego Kanamori <diegokanamori@gmail.com>
2025-12-10 10:59:58 -0300

    feat: criando menu interativo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
 import model.Book;
 import services.book.BookService;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Book book = new Book("teste1", "12/01/2023", 123, false);
-
         BookService service = new BookService();
-        service.addBook(book);
+        Scanner input = new Scanner(System.in);
 
-        System.out.println(service.listAllBooks());
+        int option = -1;
 
+        while (option != 0) {
 
+            System.out.println("----- Bem vindo ao sistema de gerenciamento de livros! -----\n");
+            System.out.println("1 - Cadastrar livro");
+            System.out.println("2 - Remover livro");
+            System.out.println("3 - Procurar livro");
+            System.out.println("4 - Listar todos os livros cadastrados");
+            System.out.println("5 - Checar Status de um livro");
+            System.out.println("6 - Gerenciar usuários");
+            System.out.println("0 - Sair");
+            System.out.print("\nDigite uma das opções acima: ");
+            option = input.nextInt();
+
+        }
 
     }
 }
