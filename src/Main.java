8f6e7b3bf8e867e3ba504ea4b7c8cf12dd7ebb5f
Diego Kanamori <diegokanamori@gmail.com>
2025-12-10 12:11:29 -0300

    feat: adicionando cadastro de livros

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import model.Book;
 import services.book.BookService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -26,7 +28,22 @@ public class Main {
             System.out.print("\nDigite uma das opções acima: ");
             option = input.nextInt();
 
+            if (option == 1) {
+                System.out.print("Digite o nome do livro: ");
+                String title = input.next();
+                System.out.print("Digite a data de lançamento: ");
+                String releaseDate = input.next();
+                System.out.print("Digite o código de fabricante: ");
+                int bookId = input.nextInt();
+                boolean isLent = false;
+                Book book = new Book(title, releaseDate, bookId, isLent);
+
+                service.addBook(book);
+
+            }
+
         }
+
 
     }
 }
