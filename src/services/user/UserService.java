3291b4a98f1868c36b5e8c5e272e9d40cef3b1d3
Diego Kanamori <diegokanamori@gmail.com>
2025-12-15 13:45:48 -0300

    feat: criando a classe user

---
+++ src/services/user/UserService.java
@@ -0,0 +1,29 @@
+package services.user;
+
+import model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService implements ManageUsers {
+
+    private Map<Integer, User> users = new HashMap<>();
+
+
+    @Override
+    public void addUsers(User user) {
+
+
+    }
+
+    @Override
+    public void removeUsers() {
+
+    }
+
+    @Override
+    public void showUsers() {
+
+    }
+
+}
